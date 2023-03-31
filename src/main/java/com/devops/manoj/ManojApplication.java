c32f8da4865504b6a5912bd1ec69af0afa2b78b9
Ram Manoj R <ramachandra.manoj.rankireddy@oracle.com>
2023-03-31 15:19:50 +0530

    Changes

---
+++ src/main/java/com/devops/manoj/ManojApplication.java
@@ -0,0 +1,13 @@
+package com.devops.manoj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManojApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManojApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devops/manoj/ManojApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devops.manoj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManojApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
